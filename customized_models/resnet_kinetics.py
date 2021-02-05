c3a72e411b7a0b332008c1df7ccccae90f6bd70b
JiaMingLin <robin890650@gmail.com>
2021-02-05 13:13:10 +0800

    add kinetics pretrain

--- customized_models/resnet_kinetics.py
+++ customized_models/resnet_kinetics.py
@@ -26,6 +26,9 @@ def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
 
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 class DistillerBasicBlock(BasicBlock):
     def __init__(self, *args, **kwargs):
