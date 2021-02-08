e4c0b9b7ec0ef47193b2257e1e70d5646df421cf
JiaMingLin <robin890650@gmail.com>
2021-02-08 13:42:12 +0800

    add quantization

---
+++ compression/create_model.py
@@ -0,0 +1,73 @@
+import sys
+sys.path.insert(0,'../')
+import copy
+from functools import partial
+import torch
+import torchvision.models as torch_models
+from torchvision.models.detection.generalized_rcnn import GeneralizedRCNN
+from torchvision.ops.misc import FrozenBatchNorm2d
+import torch.nn as nn
+import pretrainedmodels
+
+import logging
+msglogger = logging.getLogger()
+
+from models import TSN
+
+SUPPORTED_DATASETS = ['ucf101', 'hmdb51']
+
+# def create_model(pretrained, dataset, arch, num_classes, parallel=True, device_ids=None):
+def create_model(args):
+    """Create a pytorch model based on the model architecture and dataset
+
+    Args:
+        pretrained [boolean]: True is you wish to load a pretrained model.
+            Some models do not have a pretrained version.
+        dataset: dataset name (only 'imagenet' and 'cifar10' are supported)
+        arch: architecture name
+        parallel [boolean]: if set, use torch.nn.DataParallel
+        device_ids: Devices on which model should be created -
+            None - GPU if available, otherwise CPU
+            -1 - CPU
+            >=0 - GPU device IDs
+    """
+    pretrained = args.pretrained
+    dataset = args.dataset
+    num_classes = args.num_classes
+    parallel = not args.load_serialized
+    device_ids=args.gpus
+
+    dataset = dataset.lower()
+    if dataset not in SUPPORTED_DATASETS:
+        raise ValueError('Dataset {} is not supported'.format(dataset))
+
+    model = None
+    try:
+        
+        # model = models.__dict__[arch](pretrained, num_classes)
+        model = TSN(num_classes, args.num_segments, args.modality,
+                base_model=args.arch,
+                consensus_type=args.consensus_type, dropout=args.dropout, partial_bn=not args.no_partialbn)
+
+    except ValueError:
+        raise ValueError('Could not recognize dataset {} and arch {} pair'.format(dataset, arch))
+
+    msglogger.info("=> created a %s%s model with the %s dataset" % ('pretrained ' if pretrained else '',
+                                                                     arch, dataset))
+    if torch.cuda.is_available() and device_ids != -1:
+        device = 'cuda'
+        if parallel:
+            if arch.startswith('alexnet') or ('vgg' in arch):
+                model.features = torch.nn.DataParallel(model.features, device_ids=device_ids)
+            else:
+                model = torch.nn.DataParallel(model, device_ids=device_ids)
+        model.is_parallel = parallel
+    else:
+        device = 'cpu'
+        model.is_parallel = False
+
+    # Cache some attributes which describe the model
+    # _set_model_input_shape_attr(model, arch, dataset, pretrained, cadene)
+    model.arch = arch
+    model.dataset = dataset
+    return model.to(device)
---
+++ compression/parser.py
@@ -0,0 +1,65 @@
+#
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import argparse
+import distiller
+import distiller.pruning
+import distiller.models as models
+
+
+def add_cmdline_args(parser):
+    parser.add_argument('--save-untrained-model',  action='store_true', default=False,
+                        help='Save the randomly-initialized model before training (useful for lottery-ticket method)')
+    parser.add_argument('--earlyexit_lossweights', type=float, nargs='*', dest='earlyexit_lossweights', default=None,
+                        help='List of loss weights for early exits (e.g. --earlyexit_lossweights 0.1 0.3)')
+    parser.add_argument('--earlyexit_thresholds', type=float, nargs='*', dest='earlyexit_thresholds', default=None,
+                        help='List of EarlyExit thresholds (e.g. --earlyexit_thresholds 1.2 0.9)')
+    
+    parser.add_argument('--modality', '-m', metavar='MODALITY', default='rgb',
+                    	choices=["rgb", "tvl1_flow", "lk_flow"],
+                    	help='modality: rgb | flow')
+
+    parser.add_argument('--dataset', '-d', default='ucf101',
+                    	choices=["ucf101", "hmdb51"],
+                    	help='dataset: ucf101 | hmdb51')
+
+    parser.add_argument('-s', '--split', default=1, type=int, metavar='S',
+                    	help='which split of data to work on (default: 1)')
+
+    parser.add_argument('--settings', metavar='DIR', default='./settings',
+                    	help='path to datset setting files')
+
+    # parser.add_argument('--new_length', default=1, type=int,
+    #                 	metavar='N', help='length of sampled video frames (default: 1)')
+
+    parser.add_argument('--new_width', default=340, type=int,
+                    	metavar='N', help='resize width (default: 340)')
+    parser.add_argument('--new_height', default=256, type=int,
+                    	metavar='N', help='resize height (default: 256)')
+    parser.add_argument('--num_classes', default=101, type=int,
+                        metavar='N', help='classes number(default: 101)')
+    parser.add_argument('--consensus_type', type=str, default='avg',
+                    choices=['avg', 'max', 'topk', 'identity', 'rnn', 'cnn'])
+    parser.add_argument('--num_segments', type=int, default=3)
+    parser.add_argument('--dropout', '--do', default=0.5, type=float,
+                    metavar='DO', help='dropout ratio (default: 0.5)')
+    parser.add_argument('--clip-gradient', '--gd', default=None, type=float,
+                    metavar='W', help='gradient norm clipping (default: disabled)')
+    parser.add_argument('--no_partialbn', '--npb', default=False, action="store_true")
+
+    distiller.knowledge_distillation.add_distillation_args(parser, models.ALL_MODEL_NAMES, True)
+    distiller.pruning.greedy_filter_pruning.add_greedy_pruner_args(parser)
+    return parser
