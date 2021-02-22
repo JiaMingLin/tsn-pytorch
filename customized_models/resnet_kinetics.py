0d904311b2de9c177f35aa720ac078b2f7b5cd9d
JiaMingLin <robin890650@gmail.com>
2021-02-22 12:22:01 +0800

    add resnet50, 101, 152

--- customized_models/resnet_kinetics.py
+++ customized_models/resnet_kinetics.py
@@ -77,11 +77,71 @@ class DistillerBasicBlock(BasicBlock):
 
         return out
 
+class DistillerBottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion: int = 4
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+        self.add = EltwiseAdd(inplace=True)  # Replace '+=' operator with inplace module
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        # out += identity
+        out = self.add(out, identity)
+        out = self.relu(out)
+
+        return out
+
+
 class ResNet(nn.Module):
 
     def __init__(
         self,
-        block: Type[Union[BasicBlock, DistillerBasicBlock]],
+        block: Type[Union[BasicBlock, DistillerBasicBlock, DistillerBottleneck]],
         layers: List[int],
         num_classes: int = 1000,
         zero_init_residual: bool = False,
@@ -135,10 +195,10 @@ class ResNet(nn.Module):
         # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
         if zero_init_residual:
             for m in self.modules():
-                if isinstance(m, BasicBlock) or isinstance(m, DistillerBasicBlock):
+                if isinstance(m, BasicBlock) or isinstance(m, DistillerBasicBlock) or isinstance(m, DistillerBottleneck):
                     nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
 
-    def _make_layer(self, block: Type[Union[DistillerBasicBlock, BasicBlock]], planes: int, blocks: int,
+    def _make_layer(self, block: Type[Union[DistillerBottleneck, DistillerBasicBlock, BasicBlock]], planes: int, blocks: int,
                     stride: int = 1, dilate: bool = False) -> nn.Sequential:
         norm_layer = self._norm_layer
         downsample = None
@@ -186,7 +246,7 @@ class ResNet(nn.Module):
 
 def _resnet(
     arch: str,
-    block: Type[Union[BasicBlock, DistillerBasicBlock]],
+    block: Type[Union[BasicBlock, DistillerBasicBlock, DistillerBottleneck]],
     layers: List[int],
     pretrained: bool,
     progress: bool,
@@ -241,4 +301,34 @@ def resnet34_v1b_kinetics400(pretrained: bool = False, progress: bool = True, **
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
     return _resnet('resnet34_v1b_kinetics400', BasicBlock, [3, 4, 6, 3], pretrained, progress, num_classes=400,
+                   **kwargs)
+
+def resnet50(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ResNet:
+    r"""ResNet-34 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet('resnet50', DistillerBottleneck, [3, 4, 6, 3], pretrained, progress,
+                   **kwargs)
+
+def resnet101(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ResNet:
+    r"""ResNet-34 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet('resnet101', DistillerBottleneck, [3, 4, 23, 3], pretrained, progress,
+                   **kwargs)
+
+def resnet152(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ResNet:
+    r"""ResNet-34 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet('resnet152', DistillerBottleneck, [3, 8, 36, 3], pretrained, progress,
                    **kwargs)
